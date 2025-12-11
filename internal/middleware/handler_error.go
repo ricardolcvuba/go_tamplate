175107c420e3d59488275e0fcfc73e3eb37e365c
ricardolcvuba <ricardocontre27@gmail.com>
2025-12-11 18:13:48 -0300

    Add CORS and error handling middleware for improved request management

---
+++ internal/middleware/handler_error.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"errors"
+	"go_template/internal/utils/apperror"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandlerError() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		if len(c.Errors) == 0 {
+			return
+		}
+
+		last := c.Errors.Last()
+
+		var appErr apperror.AppError
+		if errors.As(last.Err, &appErr) {
+			c.AbortWithStatusJSON(appErr.Code, gin.H{
+				"code":       appErr.Code,
+				"message":    appErr.Message,
+				"status":     appErr.Status,
+				"request_id": c.GetString("request_id"),
+			})
+			return
+		}
+
+		// fallback genérico a 500 si no es un AppError tipado
+		fallback := apperror.FromCode(http.StatusInternalServerError, last.Err.Error())
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"code":       fallback.Code,
+			"message":    fallback.Message,
+			"status":     fallback.Status,
+			"request_id": c.GetString("request_id"),
+		})
+	}
+}
