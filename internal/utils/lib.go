7c6434e1590eb9ca0c98f5a93bc2b03c2d04d645
ricardolcvuba <ricardocontre27@gmail.com>
2025-12-11 09:44:00 -0300

    Refactor ToUserDTOs function to utilize ToUserDTO for improved code reuse

--- internal/utils/lib.go
+++ internal/utils/lib.go
@@ -5,14 +5,18 @@ import (
 	"go_template/internal/model"
 )
 
+func ToUserDTO(user model.User) dto.UserDTO {
+	return dto.UserDTO{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
+	}
+}
+
 func ToUserDTOs(users []model.User) []dto.UserDTO {
 	userDTOs := make([]dto.UserDTO, len(users))
 	for i, user := range users {
-		userDTOs[i] = dto.UserDTO{
-			ID:    user.ID,
-			Name:  user.Name,
-			Email: user.Email,
-		}
+		userDTOs[i] = ToUserDTO(user)
 	}
 	return userDTOs
 }
